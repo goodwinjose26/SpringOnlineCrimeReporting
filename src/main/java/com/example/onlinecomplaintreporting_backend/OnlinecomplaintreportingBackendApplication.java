165c1e5faf58775348bd6c4edd80e3458f083d7e
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-09 14:15:42 +0530

    first

---
+++ src/main/java/com/example/onlinecomplaintreporting_backend/OnlinecomplaintreportingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlinecomplaintreporting_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinecomplaintreportingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinecomplaintreportingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlinecomplaintreporting_backend/OnlinecomplaintreportingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlinecomplaintreporting_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinecomplaintreportingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
