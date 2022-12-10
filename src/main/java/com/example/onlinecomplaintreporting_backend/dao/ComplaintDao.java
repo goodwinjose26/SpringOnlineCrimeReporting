4bdb9a9f2620fc8ad96c914e73f93eb0320d4522
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-10 10:46:56 +0530

    viewmy complaint

--- src/main/java/com/example/onlinecomplaintreporting_backend/dao/ComplaintDao.java
+++ src/main/java/com/example/onlinecomplaintreporting_backend/dao/ComplaintDao.java
@@ -1,7 +1,13 @@
 package com.example.onlinecomplaintreporting_backend.dao;
 
 import com.example.onlinecomplaintreporting_backend.model.Complaints;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Map;
+
 public interface ComplaintDao extends CrudRepository<Complaints,Integer> {
+    @Query(value = "SELECT c. `address`,c. `email`,c. `name`, c. `phno`,s.compalaint FROM `customer` c JOIN complaints s on s.userid=c.id",nativeQuery = true)
+    List<Map<String,String>> Compalints();
 }
